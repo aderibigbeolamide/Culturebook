1902141bd7012fb3b422e4658a31deff32f4ec55
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-03 02:34:19 +0100

    deploy

---
+++ web-script/culturesbook.com/themes/jharah-theme/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'Jharah Theme 1';
+
+$themeFolder = 'jharah-theme';
+
+$themeAuthor = 'Jharah Global Experts';
+
+$themeAuthorUrl = 'support@jharah.com';
+
+$themeVirsion = '1.0';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
