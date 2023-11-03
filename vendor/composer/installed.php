b717e63a2b36853c60981b54ac76ae0172b6f559
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-03 04:00:37 +0100

    dependecies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'symfony/filesystem',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1902141bd7012fb3b422e4658a31deff32f4ec55',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'symfony/filesystem' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1902141bd7012fb3b422e4658a31deff32f4ec55',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webmozart/assert' => array(
+            'pretty_version' => '1.11.0',
+            'version' => '1.11.0.0',
+            'reference' => '11cb2199493b2f8a3b53e7f19068fc6aac760991',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webmozart/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
